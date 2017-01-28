89af72f5e0e1846e8057c8e2b9cc6ec6f303c7f5
Janne Harju <janne.h.harju@gmail.com>
2017-01-28 14:32:44 +0200

    Now animation is added and working. Next step might be add other Info screens and change between them for every 15s and also animate the transition

--- angular2App/app/modules/shared/navmenu/navmenu.component.ts
+++ angular2App/app/modules/shared/navmenu/navmenu.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,
+    trigger,
+  state,
+  style,
+  transition,
+  animate } from '@angular/core';
 
 @Component({
     selector: 'nav-menu',
     templateUrl: './navmenu.component.html',
     styles: [ require('./navmenu.component.css') ],
+    animations: [
+        trigger('slideInOut', [
+        state('in', style({
+            transform: 'translate3d(0, 0, 0)'
+        })),
+        state('out', style({
+            transform: 'translate3d(-100%, 0, 0)'
+        })),
+        transition('in => out', animate('400ms ease-in-out')),
+        transition('out => in', animate('400ms ease-in-out'))
+    ])]
 })
 export class NavMenuComponent {
+    menuState:string = 'out';
+ 
+    toggleMenu() {
+        // 1-line if statement that toggles the value:
+        this.menuState = this.menuState === 'out' ? 'in' : 'out';
+    }
 }
