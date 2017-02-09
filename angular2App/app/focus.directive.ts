96f815844913a1ef8731301e0a21c85239f6a169
Janne Harju <janne.h.harju@gmail.com>
2017-02-09 21:03:33 +0200

    No error but doesn’t work.

--- angular2App/app/focus.directive.ts
+++ angular2App/app/focus.directive.ts
@@ -1,7 +1,14 @@
 import { Directive, ElementRef, Input } from '@angular/core';
 @Directive({ selector: '[myFocus]' })
 export class FocusDirective {
+
+    element: ElementRef;
+
+    @Input()
+    myFocus() {
+       this.element.nativeElement.focus();
+    }
     constructor(el: ElementRef) {
-       el.nativeElement.focus();
+       this.element = el;
     }
 }
